13d0a4e58efad67a0cc598c1599cff368702b173
vhivestate <garcia.valeria001@gmail.com>
2022-04-05 21:16:27 -0600

    capture form values

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,24 @@
 var formEl = document.querySelector("#task-form"); //form id
 var tasksToDoEl = document.querySelector("#tasks-to-do"); // ul id
   
-  var createTaskHandler = function() { 
-
+  var createTaskHandler = function (event) { 
     event.preventDefault();
-    
+    var taskNameInput = document.querySelector("input[name='task-name']").value; //get task name value
+    var taskTypeInput = document.querySelector("select[name='task-type']").value; //get option value
+
+    // create list item
     var listItemEl = document.createElement("li"); 
     listItemEl.className = "task-item"; //css
-    listItemEl.textContent = "This is a new task."; 
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info"; // give it a class name
+
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl); //made new li
     }; 
   
