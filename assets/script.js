213c798f55da5e839b53b112d0bb90274d7d12ac
vhivestate <garcia.valeria001@gmail.com>
2022-04-10 19:04:10 -0600

    add ability to edit task

--- assets/script.js
+++ assets/script.js
@@ -94,16 +94,42 @@ return actionContainerEl;
 
 
   var taskButtonHandler = function(event) {
-    console.log(event.target);
+  // get target element from event
+  var targetEl = event.target;
 
-    if (event.target.matches(".delete-btn")) {
-        console.log("you clicked a delete button!");
-          // get the element's task id
-      var taskId = event.target.getAttribute("data-task-id");
-        deleteTask(taskId);
+  // edit button was clicked
+  if (targetEl.matches(".edit-btn")) {
+    var taskId = targetEl.getAttribute("data-task-id");
+    editTask(taskId);
+  } 
+  // delete button was clicked
+  else if (targetEl.matches(".delete-btn")) {
+    var taskId = targetEl.getAttribute("data-task-id");
+    deleteTask(taskId);
+  
     }
   };
-  //dlts 
+  //Edits
+  var editTask = function(taskId) {
+    console.log("editing task #" + taskId);
+  
+// get task list item element
+var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+// get content from task name and type
+var taskName = taskSelected.querySelector("h3.task-name").textContent;
+console.log(taskName);
+
+var taskType = taskSelected.querySelector("span.task-type").textContent;
+document.querySelector("input[name='task-name']").value = taskName;
+document.querySelector("select[name='task-type']").value = taskType;
+document.querySelector("#save-task").textContent = "Save Task";
+
+formEl.setAttribute("data-task-id", taskId);
+
+  };
+
+  //dlts
   var deleteTask = function(taskId) {
     var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
     taskSelected.remove();
